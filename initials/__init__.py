bc6cc38ac198273b3a06a9b81658cc13c39905b2
Holly Chaffee <saltdc95@gmail.com>
2023-02-14 12:53:25 -0500

    Initial commit

---
+++ initials/__init__.py
@@ -0,0 +1,24 @@
+# Holly Chaffee
+# I drive a mail truck
+name = """
+H   H
+H   H
+H   H
+HHHHH
+H   H
+H   H
+H   H
+"""
+print(name)
+
+name2 = """
+ CCC
+C   C
+C
+C
+C
+C   C
+ CCC
+"""
+print(name2)
+
